3c6a1604cc4d5e64bda026e7549d1aae6c2cb507
Dh-ux <lxiong19@ucsc.edu>
2023-10-10 22:25:15 -0400

    music

---
+++ buttonsound.gd
@@ -0,0 +1,35 @@
+extends AudioStreamPlayer2D
+
+@onready var buttonmusic = $"."
+# Called when the node enters the scene tree for the first time.
+
+
+func _on_bug_button_pressed():
+	buttonmusic.play()
+
+
+func _on_misting_system_button_pressed():
+	buttonmusic.play()
+
+
+func _on_open_curtains_button_pressed():
+	buttonmusic.play()
+
+
+func _on_next_day_button_pressed():
+	buttonmusic.play()
+	
+func _on_plant_button_pressed():
+	buttonmusic.play()
+
+
+func _on_flower_pressed():
+	buttonmusic.play()
+
+
+func _on_bush_pressed():
+	buttonmusic.play()
+
+
+func _on_plant_1_pressed():
+	buttonmusic.play()
--- resource/Script/bush.gd
+++ resource/Script/bush.gd
@@ -100,7 +100,6 @@ func addPlant():
 #		# Set the plant's position
 		flower_instance.position = Vector2(random_x, random_y)
 		flower_instance.growth = new_growth
-		flower_instance.scale_modifier = randf_range(0.7,1)
 		get_parent().add_child(flower_instance)
 	else:
 		print("flower_instance is null")
@@ -118,7 +117,6 @@ func on_next_day():
 		addPlant()
 		growth -= 0.5
 	var temp = remap(growth,0,2,0.1,1)
-	scale = Vector2(temp,temp)*scale_modifier
 	
 #	# Update attributes
 #	temperature = randf_range(temperature-2, temperature+2)		
